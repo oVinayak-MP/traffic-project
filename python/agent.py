5835ab15a1a5e8cd18c15d462900be4ed66798a1
Vinayak MP <vinayakmp007@gmail.com>
2018-03-16 15:58:32 +0530

    added fit function

--- python/agent.py
+++ python/agent.py
@@ -1,4 +1,6 @@
 
+
+
 import random
 import numpy as np
 from collections import deque
@@ -35,7 +37,7 @@ class DQNAgent:
 
     def merger_and_net(con1,con2): #merges convnet and also takes input state
         model=Sequential()
-        model.add(Dense(18, input_shape=(1,18,1), activation='sigmoid')) #size of state
+        model.add(Dense(18, input_shape=(1,1,18), activation='sigmoid')) #size of state
         merged = Concatenate([con1, con2])
         result = Concatenate([merged,model])
         result = Dense(530, activation='relu')
@@ -45,9 +47,9 @@ class DQNAgent:
         return results
 
     def create_model(self):                          #creates the model using fumctional API
-        first_con_input = Input(shape=(12,200,1))
-        second_con_input = Input(shape=(12,200,1))
-        third_et_input = Input(shape=(1,18))
+        first_con_input = Input(shape=(12,20,1))    #size of matrix
+        second_con_input = Input(shape=(12,20,1))   #size of matrix
+        third_et_input = Input(shape=(18,))
         model1_1=Conv2D(16, kernel_size=(3, 3), strides=(2, 2), activation='sigmoid',input_shape=(12,200,1),data_format='channels_last',padding='same')(first_con_input)
         model2_1=Conv2D(16, kernel_size=(3, 3), strides=(2, 2), activation='sigmoid',input_shape=(12,200,1),data_format='channels_last',padding='same')(second_con_input)
         model1_2=MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(model1_1)	#
@@ -60,12 +62,12 @@ class DQNAgent:
         model2_5=Flatten()(model2_4)
         model1_6=Dense(256, activation='relu')(model1_5)
         model2_6=Dense(256, activation='relu')(model2_5)
-        model3_1=Dense(18, input_shape=(1,18,1), activation='relu')(third_et_input) #size of state
-        model3_1=Flatten()(model3_1)
+        model3_1=Dense(18, activation='relu')(third_et_input) #size of state
+        #model3_2=Flatten()(model3_1)  #not used
         tempmodel=concatenate([model1_6,model2_6])
         finalemodel_1=concatenate([tempmodel,model3_1],axis=1)
         finalemodel_2=Dense(530, activation='relu')(finalemodel_1)
-        finalmodel=Dense(18, activation='softmax')(finalemodel_2)
+        finalmodel=Dense(18, activation='softmax')(finalemodel_2) #output row and column
         final=Model(inputs=[first_con_input,second_con_input,third_et_input],outputs=[finalmodel])
         final.compile(loss='categorical_crossentropy',optimizer=Adam(lr=self.learning_rate),metrics=['accuracy'])
         if self.debug == True:
@@ -73,6 +75,21 @@ class DQNAgent:
             final.summary()
         return final
 a = DQNAgent(100,200)
-a.create_model()
-
+mod=a.create_model()
+pos= np.random.rand(10000,12,20,1)
+#pos= np.random.rand(12,20)
+#pos=np.reshape(pos,(1,12,20,1))
+#speed= np.random.rand(12,20)
+#speed= np.reshape(speed,(1,12,20,1))
+speed= np.random.rand(10000,12,20,1)
+state = np.random.rand(10000,18)
+#ops = np.random.rand(18)
+ops=state
+print state
+#state=np.reshape(state,(100,1,18))
+#ops=np.reshape(ops,(1,18))
+inps={"input_1": pos,"input_2":speed,"input_3":state}
+type (inps)
+mod.fit(inps,ops,batch_size=100,epochs=100000)
+#print pos
 print('hello')
