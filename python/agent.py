feb0a181a979862a92178613f1703d9e2cfdf978
Vinayak MP <vinayakmp007@gmail.com>
2018-03-17 16:07:32 +0530

    added agent.py

--- python/agent.py
+++ python/agent.py
@@ -49,9 +49,9 @@ class DQNAgent:
     def create_model(self):                          #creates the model using fumctional API
         first_con_input = Input(shape=(12,20,1))    #size of matrix
         second_con_input = Input(shape=(12,20,1))   #size of matrix
-        third_et_input = Input(shape=(18,))
-        model1_1=Conv2D(16, kernel_size=(3, 3), strides=(2, 2), activation='sigmoid',input_shape=(12,200,1),data_format='channels_last',padding='same')(first_con_input)
-        model2_1=Conv2D(16, kernel_size=(3, 3), strides=(2, 2), activation='sigmoid',input_shape=(12,200,1),data_format='channels_last',padding='same')(second_con_input)
+        third_et_input = Input(shape=(8,))
+        model1_1=Conv2D(16, kernel_size=(3, 3), strides=(2, 2), activation='sigmoid',data_format='channels_last',padding='same')(first_con_input)
+        model2_1=Conv2D(16, kernel_size=(3, 3), strides=(2, 2), activation='sigmoid',data_format='channels_last',padding='same')(second_con_input)
         model1_2=MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(model1_1)	#
         model2_2=MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(model2_1)
         model1_3=Conv2D(32, (2, 2), activation='relu')(model1_2)
@@ -67,7 +67,7 @@ class DQNAgent:
         tempmodel=concatenate([model1_6,model2_6])
         finalemodel_1=concatenate([tempmodel,model3_1],axis=1)
         finalemodel_2=Dense(530, activation='relu')(finalemodel_1)
-        finalmodel=Dense(18, activation='softmax')(finalemodel_2) #output row and column
+        finalmodel=Dense(8, activation='softmax')(finalemodel_2) #output row and column
         final=Model(inputs=[first_con_input,second_con_input,third_et_input],outputs=[finalmodel])
         final.compile(loss='categorical_crossentropy',optimizer=Adam(lr=self.learning_rate),metrics=['accuracy'])
         if self.debug == True:
@@ -82,7 +82,7 @@ pos= np.random.rand(10000,12,20,1)
 #speed= np.random.rand(12,20)
 #speed= np.reshape(speed,(1,12,20,1))
 speed= np.random.rand(10000,12,20,1)
-state = np.random.rand(10000,18)
+state = np.random.rand(10000,8)
 #ops = np.random.rand(18)
 ops=state
 print state
